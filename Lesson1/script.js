481c14ef2e41a931c189792ef571ffdcc27ddcd4
Otabek <otabek.badritdinov@mail.ru>
2018-09-06 17:55:09 +0500

    Fourth commit

---
+++ Lesson1/script.js
@@ -0,0 +1,10 @@
+var name = prompt('Hello! What is your name');
+alert('Ok, ' + name);
+
+var budget = prompt('Please tell us what is your budget?');
+alert('Good');
+
+alert('Now write in the form your daily earnings and expenses and we try to optimize your budget');
+
+
+
